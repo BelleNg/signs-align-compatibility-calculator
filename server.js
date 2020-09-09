8e0297b1d4b99ddd51762011f8d1ab24999d0926
Belle Nguyen <NgBelleK@gmail.com>
2020-09-09 11:32:36 -0500

    install cors

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
- 
+const cors = require('cors');
 const PORT = 3000;
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT);
+app.use(express.static(__dirname + '/public'))
  
 app.get('/', (req, res) => {
  res.send({working: true})
 })
+
+app.listen(PORT, function () {
+    console.log('Server listening on port:',PORT);
+  })
