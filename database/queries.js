827cec16aecae8fba93510b7402761d1f07e2202
Belle Nguyen <NgBelleK@gmail.com>
2020-09-09 16:02:40 -0500

    create working routes for year and score

--- database/queries.js
+++ database/queries.js
@@ -1,22 +1,54 @@
-const mysql = require('mysql');
-const databaseConfig = require('./db.config.js');
+const mysql = require("mysql");
+const databaseConfig = require("./db.config.js");
 
 const connection = mysql.createConnection(databaseConfig);
 
+//used to test if server is connected to database
 const testDbConnect = (callback) => {
-  connection.query('SELECT animal FROM zodiac', (err, results) => {
+  connection.query("SELECT animal FROM zodiac", (err, results) => {
     if (err) {
-      console.log('Error in testDbConnect', err);
+      console.log("Error in testDbConnect", err);
       callback(err, null);
     } else {
-      console.log('Success from testDbConnect');
+      console.log("Success from testDbConnect");
       callback(null, results);
     }
   });
 };
 
+//Make sure id is an integer
+const getZodiacAnimal = (year, callback) => {
+  connection.query(
+    `SELECT animal FROM zodiac WHERE id = ${year}`,
+    (err, results) => {
+      if (err) {
+        console.log("Query error in getting zodiac by year", err);
+        callback(err, null);
+      } else {
+        console.log("Success query for zodiac");
+        callback(null, results);
+      }
+    }
+  );
+};
 
+const getCoupleScore = (couple, callback) => {
+  connection.query(
+    `SELECT score FROM couple_score WHERE couple = '${couple}'`,
+    (err, results) => {
+      if (err) {
+        console.log("Query error in getting couple score", err);
+        callback(err, null);
+      } else {
+        console.log("Success query for couple score");
+        callback(null, results);
+      }
+    }
+  );
+};
 
 module.exports = {
   testDbConnect,
+  getZodiacAnimal,
+  getCoupleScore,
 };
--- server.js
+++ server.js
@@ -1,29 +1,63 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const db = require('./database/queries.js');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const db = require("./database/queries.js");
 const PORT = 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname + '/public'))
- 
-app.get('/hi', (req, res) => {
- res.send({working: true})
-})
+app.use(express.static(__dirname + "/public"));
 
-app.get('/testBackendConnection', (req, res) => {
-    db.testDbConnect((err, results) => {
+app.get("/hi", (req, res) => {
+  res.send({ working: true });
+});
+
+app.get("/testBackendConnection", (req, res) => {
+  db.testDbConnect((err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+app.get("/api/zodiac/:year", (req, res) => {
+  let yearNum = parseInt(req.params.year);
+  db.getZodiacAnimal(yearNum, (err, results) => {
+    if (err) {
+      console.log("Error in price route");
+      res.sendStatus(400);
+    } else {
+      if (results.length === 0) {
+        console.log("year not in database");
+        res.send({ animal: 'mantis shrimp' });
+      }
+      console.log("Successful price query");
+      res.status(200).send(results[0]);
+    }
+  });
+});
+
+app.get("/api/score/:couple", (req, res) => {
+    let couple = req.params.couple;
+    db.getCoupleScore(couple, (err, results) => {
       if (err) {
-        res.send(err);
+        console.log("Error in price route");
+        res.sendStatus(400);
       } else {
-        res.send(results);
+        if (results.length === 0) {
+          console.log("year not in database");
+          res.send({ score: 120 });
+        }
+        console.log("Successful price query");
+        res.status(200).send(results[0]);
       }
     });
   });
 
 app.listen(PORT, function () {
-    console.log('Server listening on port:',PORT);
-  })
+  console.log("Server listening on port:", PORT);
+});
