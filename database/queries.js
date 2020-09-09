f20b55e10c6496cc0b75db0cdcba165871e5572c
Belle Nguyen <NgBelleK@gmail.com>
2020-09-09 11:41:47 -0500

    install mysql package and template for file

---
+++ database/queries.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+const databaseConfig = require('./db.config.js');
+
+const connection = mysql.createConnection(databaseConfig);
+
+// const testDbConnect = (callback) => {
+//   connection.query('SELECT price FROM products', (err, results) => {
+//     if (err) {
+//       console.log('Error in testDbConnect', err);
+//       callback(err, null);
+//     } else {
+//       console.log('Success from testDbConnect');
+//       callback(null, results);
+//     }
+//   });
+// };
+
+
+
+module.exports = {
+  testDbConnect,
+};
