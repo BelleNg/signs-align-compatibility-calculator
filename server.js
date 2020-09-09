e60d73ee212ef5a27fe53d796a367e623724f12c
Belle Nguyen <NgBelleK@gmail.com>
2020-09-09 16:03:28 -0500

    fix console.log

--- server.js
+++ server.js
@@ -45,14 +45,14 @@ app.get("/api/score/:couple", (req, res) => {
     let couple = req.params.couple;
     db.getCoupleScore(couple, (err, results) => {
       if (err) {
-        console.log("Error in price route");
+        console.log("Error in score route");
         res.sendStatus(400);
       } else {
         if (results.length === 0) {
           console.log("year not in database");
           res.send({ score: 120 });
         }
-        console.log("Successful price query");
+        console.log("Successful score query");
         res.status(200).send(results[0]);
       }
     });
